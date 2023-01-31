dd0a737f5b73ec27dd3add16b3fc239a37afbc84
PETIC <edgar.ulisesrdr@gmail.com>
2023-01-30 22:25:00 -0600

    creacion de ejercicio tema 3

---
+++ src/com/EjerciciosTema3/concatenar.java
@@ -0,0 +1,15 @@
+package com.EjerciciosTema3;
+
+public class concatenar {
+    public static void main(String[] args) {
+        conca();
+    }
+
+    static void conca(){
+        String gatos [] = {"Coco", "Hayley", "Calixto"};
+
+        for (int i=0;i<=0 ;i++){
+            System.out.println(gatos[0]+" "+gatos[1]+" "+gatos[2]);
+        }
+    }
+}
